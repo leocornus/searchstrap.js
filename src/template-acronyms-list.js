966cb61c9780a3e82e7d3fd43410e658aa5b9bc5
Sean Chen <sean.chen@leocorn.com>
2017-06-21 12:52:03 -0400

    create separate file for profiles list

--- src/template-acronyms-list.js
+++ src/template-acronyms-list.js
@@ -9,72 +9,6 @@ var buildAcronymsList = function(strap, $result, docs, currentQuery,
                           acronymPanelStripper);
 };
 
-//var buildAcronymsList = function(strap, $result, docs, currentQuery,
-//                     total, currentPage, totalPages, pagination) {
-//
-//    return buildXColsARow(strap, $result, docs, currentQuery, total,
-//                   currentPage, totalPages, pagination, 4,
-//                   acronymPanelStripper);
-//};
-
-var buildAcronymsListOld = function(strap, $result, docs, currentQuery,
-                     total, currentPage, totalPages, pagination) {
-
-    var resultSummary = '';
-    if(total > 0) {
-        var end = currentQuery.start +
-                  currentQuery.perPage - 1;
-        end = end > total ? total : end;
-        resultSummary =
-            'Page <strong>' + currentPage + '</strong>' +
-            ' Showing [<strong>' + currentQuery.start +
-            '</strong> - <strong>' + end +
-            '</strong>] of <strong>' +
-            total + '</strong> total results';
-    } else {
-        // no result found
-        resultSummary =
-            '<strong>No results containing ' +
-            'all your search terms were found.</strong>';
-    }
-    $('#search-info').html(resultSummary);
-
-    // build a 6 columns to show 
-    $result.html("");
-
-    //result.append('<div>' + pagination + '</div>');
-    var colQueue =[];
-    for(i = 0; i < docs.length; i++) {
-        var acronym = docs[i];
-        //var panel = acronymPanelStripper(acronym);
-        var panel = userProfilePanel(acronym);
-        colQueue.push(panel);
-        // i count from 0
-        // 6 acronyms for a row
-        var ready2Row = (i + 1) % 4;
-        if(ready2Row == 0) {
-            $result.append('<div class="row">' +
-                colQueue.join("") + '</div>');
-            // reset the queue.
-            colQueue = [];
-        }
-    }
-
-    // check if we missed anything...
-    if(colQueue.length > 0) {
-
-        // append to the last row.
-        $result.append('<div class="row">' +
-            colQueue.join(" ") +
-            '</div>');
-    }
-
-    // add the pagination at the bottom too.
-    $result.append('<div>' + pagination + '</div>');
-
-    return $result;
-};
-
 /**
  * builder function to strip out all wiki syntax.
  */
@@ -97,53 +31,5 @@ var acronymPanelStripper = function(acronym) {
         acronym['title'] + '</a></h2>' +
         '<p><ul class="list-group">' + text + '</li></ul></p>' + 
         '</div>';
-    return panel;
-};
-
-/**
- * builder function to build the user profile by using the 
- * Bootstrap thumbnail.
- */
-var userProfilePanel = function(profile) {
-
-   console.log(profile);
-
-    // try to remove some wiki markups.
-    var desc = profile['content'];
-
-    // thumbnail panel
-    var panel =
-'<div class="thumbnail" style="border: 0px">' +
-'  <a href="' + profile['url'] + '">' +
-'  <img src="' + profile['image'] + 
-            '" class="img-circle" width=100% alt="' + 
-            profile['title'] + '"/>' +
-'  </a>' +
-'  <div class="caption">' +
-'    <h3><a href="' + profile['url'] + '">' + profile['title'] + 
-'</a></h3>' +
-'    <p>' + desc +'</p>' +
-'  </div>' +
-'</div>';
-
-    // using the pull-left try to make the text wrap around the
-    // image.
-    panel =
-'<div>' +
-'  <a class="pull-left" href="' + profile['url'] + '">' +
-'  <img src="' + profile['image'] + 
-            '" class="img-circle" width=86 alt="' + 
-            profile['title'] + '"/>' +
-'  </a>' +
-'  <div>' +
-'    <h4><a href="' + profile['url'] + '">' + profile['title'] + 
-'</a></h4>' +
-'    <p>' + desc +'</p>' +
-'  </div>' +
-'</div>';
-
-    // add the column div.
-    panel = '<div class="col-sm-3">' + panel + '</div>'
-
     return panel;
 };
---
+++ src/template-profiles-list.js
@@ -0,0 +1,58 @@
+/**
+ * build the Acronyms list, which will have 6 columns
+ */
+var buildProfilesList = function(strap, $result, docs, currentQuery,
+                     total, currentPage, totalPages, pagination) {
+
+    return buildXColsARow(strap, $result, docs, currentQuery, total,
+                          currentPage, totalPages, pagination, 4,
+                          userProfilePanel);
+};
+
+/**
+ * builder function to build the user profile by using the 
+ * Bootstrap thumbnail.
+ */
+var userProfilePanel = function(profile) {
+
+   console.log(profile);
+
+    // try to remove some wiki markups.
+    var desc = profile['content'];
+
+    // thumbnail panel
+    var panel =
+'<div class="thumbnail" style="border: 0px">' +
+'  <a href="' + profile['url'] + '">' +
+'  <img src="' + profile['image'] + 
+            '" class="img-circle" width=100% alt="' + 
+            profile['title'] + '"/>' +
+'  </a>' +
+'  <div class="caption">' +
+'    <h3><a href="' + profile['url'] + '">' + profile['title'] + 
+'</a></h3>' +
+'    <p>' + desc +'</p>' +
+'  </div>' +
+'</div>';
+
+    // using the pull-left try to make the text wrap around the
+    // image.
+    panel =
+'<div>' +
+'  <a class="pull-left" href="' + profile['url'] + '">' +
+'  <img src="' + profile['image'] + 
+            '" class="img-circle" width=86 alt="' + 
+            profile['title'] + '"/>' +
+'  </a>' +
+'  <div>' +
+'    <h4><a href="' + profile['url'] + '">' + profile['title'] + 
+'</a></h4>' +
+'    <p>' + desc +'</p>' +
+'  </div>' +
+'</div>';
+
+    // add the column div.
+    panel = '<div class="col-sm-3">' + panel + '</div>'
+
+    return panel;
+};
