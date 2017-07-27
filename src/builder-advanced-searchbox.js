245a5e7824531ca9c95efe730f458d30fc2cf2b9
Sean Chen <sean.chen@leocorn.com>
2017-07-27 15:18:07 -0400

    create the demo page for advanced search box.

---
+++ src/builder-advanced-searchbox.js
@@ -0,0 +1,66 @@
+/**
+ * the default builder to build search input box
+ * this will depend on Bootstrap
+ */
+var searchBoxWithDropdown = function(strap) {
+
+    // build the search box.
+    var searchBox = 
+'<div class="input-group input-group-lg"' +
+'     role="group" aria-label="...">' +
+'  <div class="form-group form-group-lg has-feedback has-clear">' +
+'    <input type="text" class="form-control"' +
+'           placeholder="' + strap.settings.placeholder + '"' +
+'           id="search-input"' +
+'           aria-describedby="sizing-addon"/>' +
+'    <span class="form-control-clear text-danger' +
+'                 glyphicon glyphicon-remove' +
+'                 form-control-feedback hidden"></span>' +
+'  </div>' +
+'  <span class="input-group-addon" id="search-button"' +
+'        style="cursor: pointer">' +
+'    <span class="glyphicon glyphicon-search ' +
+'                 text-primary"></span> Search' +
+'  </span>' +
+'</div>' +
+// the info bar. summary of search result.
+'<div class="text-muted h4" id="search-info">' +
+'  <h2>Loading...</h2>' +
+'</div>';
+
+    // replace the search box.
+    strap.$element.html('').append(searchBox);
+    // set up the searchInput jQuery object..
+    strap.$searchInput = strap.$element.find('input');
+    // set up the searchButton jQuery object
+    strap.$searchButton = 
+        strap.$element.find('#search-button');
+    // set up the search summary jQuery object.
+    strap.$searchSummary = 
+        strap.$element.find('#search-info');
+
+    /**
+     * hook the clik event on the remove icon.
+     * It will need the following styles to work fine on IE
+     *
+     * ::-ms-clear {
+     *   display: none;
+     * }
+     *
+     * .form-control-clear {
+     *   z-index: 10;
+     *   pointer-events: auto;
+     *   cursor: pointer;
+     * }
+     */
+    strap.$element.find('.glyphicon-remove')
+        .on('click', function(event) {
+
+        // clean the search term in the input box.
+        strap.$searchInput.val('');
+        // hide the remove icon.
+        $(this).addClass('hidden');
+        // trigger search and reload page.
+        strap.handleButtonClick();
+    });
+};
