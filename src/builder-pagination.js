4b00839836c901479e71ce241bbd36754cc144fb
Sean Chen <sean.chen@leocorn.com>
2017-08-18 11:46:42 -0400

    add the default pagination builder in French

---
+++ src/builder-pagination.js
@@ -0,0 +1,13 @@
+/**
+ * build the default pagination in French.
+ */
+var defaultPaginationDotsFR = function(strap, currentPage, totalPages) {
+
+    var labels = {
+        'previous' : '&laquo; Précédent',
+        'next' : 'Prochain &raquo;'
+    };
+
+    return strap.defaultPaginationDots(strap, currentPage, totalPages, 
+                                       labels);
+}
