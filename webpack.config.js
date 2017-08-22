100196798428c4fd7551b7964bbc1499fde9c6a0
Sean Chen <sean.chen@leocorn.com>
2017-08-22 11:11:05 -0400

    webpack config to pack defaultTemplates and moment with fr-ca support

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,15 @@ var glob = require('glob');
 module.exports = {
 
   // using glob.sync to support wildcard mapping!
-  entry: glob.sync('./src/@(template|layout|builder)*.js'),
+  entry: {
+    defaultTemplates: glob.sync('./src/@(template|layout|builder)*.js'),
+    moment: [
+      './bower_components/moment/moment.js',
+      './bower_components/moment/locale/fr-ca.js'
+    ]
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'demo')
   }
 };
