8642b5285b4a1c0b1cc28ff5b4a2e258c59208e3
Sean Chen <sean.chen@leocorn.com>
2017-08-23 10:22:28 -0400

    add comments for the webpack config js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 const path = require('path');
 
+// using glob.sync to support wildcard mapping!
 var glob = require('glob');
-var templates = glob.sync('./src/@(template|layout|builder)*.js');
-templates.push('./bower_components/moment/moment.js');
-templates.push('./bower_components/moment/locale/fr-ca.js');
+// we need an array for a multiple list of entry files.
+var templates = new Array();
+// the concat function will lile the push in PHP.
+templates = templates.concat(glob.sync('./src/@(template|layout|builder)*.js'));
 
 module.exports = {
 
-  // using glob.sync to support wildcard mapping!
   entry: {
+    // the output bundle file will have name 
+    // defaultTemplates.js
     defaultTemplates: templates,
-    moment: [
-      './bower_components/moment/moment.js',
-      './bower_components/moment/locale/fr-ca.js'
-    ]
+    //moment: [
+    //  './bower_components/moment/src/moment.js'
+    //]
   },
   output: {
+    // set up the file name pattern here
     filename: '[name].js',
+    // we will save the output bundle file in demo folder
     path: path.resolve(__dirname, 'demo')
   }
 };
